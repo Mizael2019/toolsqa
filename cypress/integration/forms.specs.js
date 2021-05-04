dd4101b7bf74b4db11b94375573cb59b6e2575e6
Mizael Nazareno <mizael.nazareno@useblu.com.br>
2021-05-04 16:39:32 -0300

    ajustes testes

--- cypress/integration/forms.specs.js
+++ cypress/integration/forms.specs.js
@@ -7,8 +7,8 @@ describe('Acesso página principal e submete os campos', () => {
     });
 
     it('Preencher os campos', () => {
-        cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').click()
-        cy.get(':nth-child(1) > .element-list > .menu-list > #item-0').click()
+        cy.get('.card-body').contains('Elements').click()
+        cy.get('#item-0').click()
         cy.get('#userName').as('name').type('Mizael Tobias Nazareno')
         cy.get('#userEmail').as('email').type('mtszareno@gmail.com')
         cy.get('#currentAddress').as('currentAddress').type('Rio de Janeiro / BR')
@@ -24,12 +24,12 @@ describe('Acesso página principal e submete os campos', () => {
 describe('Teste de envio de Formulário', () => {
     beforeEach(() => cy.visit('/'));
     it('Preenche os campos do Form', () => {
-        cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').click()
+        cy.get('.card-body').contains('Elements').click()
         cy.get(':nth-child(2) > .group-header > .header-wrapper').click()
         cy.get(':nth-child(2) > .element-list > .menu-list > #item-0').click()
         cy.get('#firstName').as('firstname').type('Carlos')
         cy.get('#lastName').as('lastname').type('Nazareno')
-        cy.get('#userEmail').as('email').type('mtszareno@gmail.com')
+        cy.get('#userEmail').as('email').type('humildade@gmail.com')
         cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
         cy.get('#userNumber').type('2222222222')
         cy.get('#dateOfBirthInput').click()
@@ -52,15 +52,15 @@ describe('Teste de envio de Formulário', () => {
 describe('Teste de tabelas', () => {
     beforeEach(() => cy.visit('/'));
     it('Preenche um usuário e deleta em seguida', () => {
-        cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').click()
-        cy.get(':nth-child(1) > .element-list > .menu-list > #item-3').click()
+        cy.get('.card-body').contains('Elements').click()
+        cy.get('#item-3').click()
         cy.get('#addNewRecordButton').click()
         cy.get('#firstName').as('firstname').type('Pedro')
         cy.get('#lastName').as('lastname').type('Camargo')
         cy.get('#userEmail').as('email').type('salvesalve@gmail.com')
-        cy.get('#age').type('37')
-        cy.get('#salary').type('2000')
-        cy.get('#department').as('department').type('Motors')
+        cy.get('#age').type('20')
+        cy.get('#salary').type('2200')
+        cy.get('#department').as('department').type('Lojística')
         cy.get('@firstname').should('be.empty')
         cy.get('@lastname').should('be.empty')
         cy.get('@email').should('be.empty')
