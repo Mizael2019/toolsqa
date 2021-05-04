976bb548b5b85ce8eb3c7a66b560693e7639fd25
Mizael Nazareno <mizael.nazareno@useblu.com.br>
2021-05-04 13:19:20 -0300

    criaçãoformulário testesde

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# Estudos Tools QA
+# Tools QA
 
-- Repo utlizado para demostrar conhecimentos de automação de testes.
+- Repo utlizado para demostrar conhecimentos em automação de testes.
+
+📢 Estudos Tools QA </br>
+
+Repositório de automação de testes E2E Web com Cypress
+
+## 🛠 Tecnologias
+
+Cypress</br>
+Node.js </br>
+Javascript </br>
+
+## 🎯 Pré-Requisitos
+
+<b>Clonar o projeto</b></br>
+
+<b>Instalar as dependências do package.json </b> </br>
+$ npm install</br>
+
+Done ✔️
+
+## 💡 Uso
+
+<b>Como Rodar os testes: </b></br>
+
+<b>Rodar testes diretamente no browser</b></br>
+$ npx cypress open</br>
+
+<b>Rodar os testes em segundo plano (headless)</b></br>
+$ npx cypress run </br>
--- cypress/integration/forms.specs.js
+++ cypress/integration/forms.specs.js
@@ -1,10 +1,10 @@
-describe('Acesso página principal e clica em Elements', () => {
-   beforeEach(() => cy.visit('https://demoqa.com/'));
-   
+/// <reference types="cypress" />
+
+describe('Acesso página principal e submete os campos', () => {
+    beforeEach(() => cy.visit('/'));
     it('Titulo página visível', () => {
         cy.get('[src="/images/Toolsqa.jpg"]').should('be.visible')
     });
-
 
     it('Preencher os campos', () => {
         cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').click()
@@ -19,35 +19,54 @@ describe('Acesso página principal e clica em Elements', () => {
         cy.get('@currentAddress').should('be.empty')
         cy.get('@permanAddress').should('be.empty')
     });
+});
 
-
-    it('Preencher o Forms', () => {
+describe('Teste de envio de Formulário', () => {
+    beforeEach(() => cy.visit('/'));
+    it('Preenche os campos do Form', () => {
         cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').click()
         cy.get(':nth-child(2) > .group-header > .header-wrapper').click()
         cy.get(':nth-child(2) > .element-list > .menu-list > #item-0').click()
-
         cy.get('#firstName').as('firstname').type('Carlos')
         cy.get('#lastName').as('lastname').type('Nazareno')
         cy.get('#userEmail').as('email').type('mtszareno@gmail.com')
-        cy.get('#genterWrapper > .col-md-9 > :nth-child(2) > .custom-control-label').click()
+        cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
         cy.get('#userNumber').type('2222222222')
         cy.get('#dateOfBirthInput').click()
         cy.get('.react-datepicker__day--020').click()
-        cy.get('.subjects-auto-complete__value-container').click()
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
         cy.get('#currentAddress').as('currentAddress').type('Rio de Janeiro / BR')
-        
         cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click()
         cy.get('#react-select-3-option-0').click()
         cy.get('#city > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click()
         cy.get('#react-select-4-option-0').click()
-
         cy.get('#submit').click()
         cy.get('#closeLargeModal').click()
-        
         cy.get('@firstname').should('be.empty')
         cy.get('@lastname').should('be.empty')
         cy.get('@email').should('be.empty')
         cy.get('@currentAddress').should('be.empty')
+    });
+});
+
+describe('Teste de tabelas', () => {
+    beforeEach(() => cy.visit('/'));
+    it('Preenche um usuário e deleta em seguida', () => {
+        cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').click()
+        cy.get(':nth-child(1) > .element-list > .menu-list > #item-3').click()
+        cy.get('#addNewRecordButton').click()
+        cy.get('#firstName').as('firstname').type('Pedro')
+        cy.get('#lastName').as('lastname').type('Camargo')
+        cy.get('#userEmail').as('email').type('salvesalve@gmail.com')
+        cy.get('#age').type('37')
+        cy.get('#salary').type('2000')
+        cy.get('#department').as('department').type('Motors')
+        cy.get('@firstname').should('be.empty')
+        cy.get('@lastname').should('be.empty')
+        cy.get('@email').should('be.empty')
+        cy.get('@department').should('be.empty')
+        cy.get('#submit').click()
+        cy.wait(2000)
+        cy.get('#delete-record-4 > [stroke="currentColor"]').click()
     });
 });
